0583cbbae9ce93b9f72581101c1a5000c69039cf
Maksym Nesterov <maxym.nes.dev@gmail.com>
2021-11-27 16:07:45 +0200

    Add avatar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,16 @@ import { AppRoutingModule } from '@/app-routing.module';
 import { AppComponent } from '@/app.component';
 import { UsersListComponent } from '@/pages/users-list/users-list.component';
 import { LearningsListComponent } from '@/pages/learnings-list/learnings-list.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationComponent } from '@/components/navigation/navigation.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersListComponent,
     LearningsListComponent,
-    NavigationComponent
+    NavigationComponent,
+    AvatarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/avatar/avatar.component.ts
@@ -0,0 +1,36 @@
+import { UserEntry } from '@/services/users/users.types';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.scss'],
+})
+export class AvatarComponent implements OnInit {
+  @Input() private source: UserEntry;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  get image() {
+    return this.source.avatar;
+  }
+
+  get owner() {
+    return this.source.name;
+  }
+
+  get abbreviation() {
+    const words = this.source.name.split(' ');
+    let res = this._getFirstChar(words[0]);
+    if (words.length > 1) {
+      res += this._getFirstChar(words[words.length - 1]);
+    }
+    return res;
+  }
+
+  private _getFirstChar(str: string) {
+    return str.charAt(0).toUpperCase();
+  }
+}
